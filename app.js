6b94124a998a5623f432b28ddb3ab29d4f61c184
shinrai06 <shivareddyc.cs20@rvce.edu.in>
2022-09-21 14:52:44 +0530

    POST and STORE
    Set up a new page to add the tasks, the added task will be stored in the data.json file automatically

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const data = require("./data.json");
+
+const fs = require('fs');
+var data = fs.readFileSync("data.json");
+var ob = JSON.parse(data);
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}))
@@ -9,15 +12,34 @@ app.use(bodyParser.json());
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'))
 
-app.get('/',(req,res) => {
+app.get('/home',(req,res) => {
     res.render('home.ejs');
+})
+app.post('/home',(req,res) => {
+    console.log("POST request");
+    res.redirect('index');
 })
 
 app.get('/index', (req,res) => {
-    const tasks = data.Users.Tasks;
+    const tasks = ob.Users.Tasks;
     console.log(tasks);
     res.render('index.ejs', { tasks });
+    //res.send("Index Page");
 })
+app.get('/new', (req, res) => {
+    res.render('addTask.ejs');
+})
+app.post('/new', (req, res) => {
+    const { task } = req.body;
+    ob.Users.Tasks.push(task);
+    var ob2 = JSON.stringify(ob);
+    fs.writeFile("data.json", ob2, (err) => {
+        if(err) throw err;
+        console.log(ob);
+    })
+    res.redirect('index');
+})
+
 app.listen(3000, () => {
     console.log("localHost: 3000");
 })
