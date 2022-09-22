91191a93740505fdc53ff0906c83ca50ca52262d
shinrai06 <shivareddyc.cs20@rvce.edu.in>
2022-09-22 12:07:43 +0530

    Added validation

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const fs = require("fs");
 var data = fs.readFileSync("data.json");
 var ob = JSON.parse(data);
+var users = ob.Users;
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -13,7 +14,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
 const AddNewTask = async (task) => {
-  ob.Users.Tasks.push(task);
+  users.Tasks.push(task);
   var ob2 = JSON.stringify(ob);
   fs.writeFile("data.json", ob2, (err) => {
     if (err) 
@@ -21,17 +22,28 @@ const AddNewTask = async (task) => {
     console.log(ob);
   });
 }
-
+const validate = (x) => {
+  console.log(x.userName,x.email,x.password);
+  console.log(users.UserName,users.Email,users.Password);
+  if((x.userName === users.UserName) && (x.email === users.Email) && (x.password == users.Password)){
+    return true;
+  }
+  return false;
+}
 app.get("/home", (req, res) => {
   res.render("home.ejs");
 });
 app.post("/home", (req, res) => {
-  res.redirect("index");
+  const x = req.body;
+  if(validate(x))
+    res.redirect("index");
+  else
+    res.send("Invalid Crediantals!!!");
 });
 
 app.get("/index", (req, res) => {
-  const tasks = ob.Users.Tasks;
-  console.log(tasks);
+  const tasks = users.Tasks;
+  console.log(typeof(tasks));
   res.render("index.ejs", { tasks });
 });
 
